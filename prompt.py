7f6017bfd472e6c2a77c86ea29e5b9336c162ead
Tharunraj Optimus <tharunoptimus@outlook.com>
2023-06-07 23:48:43 +0530

    Update prompt.py
    - Added print statements
    - Added time

--- prompt.py
+++ prompt.py
@@ -1,12 +1,16 @@
+print("Importing Necessary Libraries", end="\n")
+
 # Load the required libraries
 # from transformers import T5ForConditionalGeneration, T5Tokenizer
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 import os
+import time
 
 # The base model that is to be fine-tuned with alpaca dataset
 base_model_name = "currentlyexhausted/lite-llm"
 
+print("Loading the model... Please wait", end="\n")
 # Load the pre-trained flan T5 model and tokenizer
 model = T5ForConditionalGeneration.from_pretrained(base_model_name)
 tokenizer = T5Tokenizer.from_pretrained(base_model_name)
@@ -18,9 +22,13 @@ print(device.type, "is available")
 print(torch.get_num_threads(), "threads available!")
 
 def litellm():
-  input_text = input("Enter your prompt")
+  input_text = input("Enter your prompt: ")
+  
+  start = time.process_time()
+  print("Tokenizing your prompt for the model...", end="\n")
   input_ids = tokenizer.encode(input_text, return_tensors='pt').to(device)
 
+  print("Model is now generating... ", end="\n")
   # Generate the output with modified settings
   output = model.generate(
       input_ids=input_ids,
@@ -32,7 +40,10 @@ def litellm():
       temperature=0.5 # Control the level of randomness
   )
 
+  print("Decoding the tokens to generate output...", end="\n\n")
   # Convert the output token ids to text
-  return tokenizer.decode(output[0], skip_special_tokens=True)
+  output = tokenizer.decode(output[0], skip_special_tokens=True)
+  print("Took: ", time.process_time() - start)
+  return output
 
 print(litellm())
