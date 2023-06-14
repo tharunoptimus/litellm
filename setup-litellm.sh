59e02286d3619033c4f73a98657739e21e0f26b5
Tharunraj Optimus <tharunoptimus@outlook.com>
2023-06-14 10:20:52 +0530

    Create setup-litellm.sh

---
+++ setup-litellm.sh
@@ -0,0 +1,13 @@
+pkg upgrade
+pkg i wget proot git -y
+cd ~
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+cd ubuntu-in-termux
+chmod +x ubuntu.sh
+./ubuntu.sh -y
+./startubuntu.sh
+apt update
+apt install wget
+wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/setup-llm.sh -o setup-llm.sh
+chmod +x setup-llm.sh
+./setup-llm.sh
