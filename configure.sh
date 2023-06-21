018d5daf33459d3142545dd3ed02f6e8cadf5ac7
Tharunraj Optimus <tharunoptimus@outlook.com>
2023-06-21 19:20:46 +0530

    Update and rename setup-litellm.sh to configure.sh

--- configure.sh
+++ configure.sh
@@ -8,6 +8,6 @@ chmod +x ubuntu.sh
 ./startubuntu.sh
 apt update
 apt install wget
-wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/setup-llm.sh -o setup-llm.sh
+wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/setup.sh -o setup.sh
 chmod +x setup-llm.sh
 ./setup-llm.sh
