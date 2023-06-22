535bbc36db461330d516c8fb9a0735fda4571277
Tharunraj Optimus <tharunoptimus@outlook.com>
2023-06-22 22:08:36 +0530

    Update setup.sh
    - Added ascii art
    - Added script to download upd-files.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-echo "Downloading Python and other dependencies for setting the environment..."
+printf "Downloading Python and other dependencies for setting the environment...\n"
 apt-get install wget python3 python3-venv -y
 
-echo "Creating the environment..."
+printf "Creating the environment...\n"
 python3 -m venv litellm
 source litellm/bin/activate
 
-echo "Downloading and installing libraries for running the models"
+printf "Downloading and installing libraries for running the models...\n\n"
 pip install torch
 pip install transformers
 pip install sentencepiece
 pip install fastapi
 pip install uvicorn
 
-echo "Downloading the source for the server..."
-echo "You will have to download about 400MB during first run..."
+printf "Downloading the source for the server...\n"
+printf "You will have to download about 400MB during first run...\n"
 
 echo "Downloading the required files for the app..."
 wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/service.py -O service.py
 wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/index.html -O index.html
+wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/upd-files.sh -O upd-files.sh
 mkdir static
 cd static && wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/static/style.css -O style.css
 cd static && wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/static/app.js -O app.js
 
-echo "Installation complete. Run `python3 service.py` to start the service"
+printf \n
+printf \n
+ printf _      _  _          _      _     ___  ___\n
+printf | |    (_)| |        | |    | |    |  \/  |\n
+printf | |     _ | |_   ___ | |    | |    | .  . |\n
+printf | |    | || __| / _ \| |    | |    | |\/| |\n
+printf | |____| || |_ |  __/| |____| |____| |  | |\n
+printf \_____/|_| \__| \___|\_____/\_____/\_|  |_/\n
+printf \n                                           
+printf \n
+
+printf "Installation complete. Run `python3 service.py` to start the service"
