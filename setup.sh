7a0a01206a165f7ec70d71e0eaf511c04736b618
Tharunraj Optimus <tharunoptimus@outlook.com>
2023-06-22 22:14:43 +0530

    Bug Fix - Added `"` to `printf`

--- setup.sh
+++ setup.sh
@@ -25,15 +25,15 @@ mkdir static
 cd static && wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/static/style.css -O style.css
 cd static && wget https://raw.githubusercontent.com/tharunoptimus/litellm/main/static/app.js -O app.js
 
-printf \n
-printf \n
-printf _      _  _          _      _     ___  ___\n
-printf | |    (_)| |        | |    | |    |  \/  |\n
-printf | |     _ | |_   ___ | |    | |    | .  . |\n
-printf | |    | || __| / _ \| |    | |    | |\/| |\n
-printf | |____| || |_ |  __/| |____| |____| |  | |\n
-printf \_____/|_| \__| \___|\_____/\_____/\_|  |_/\n
-printf \n                                           
-printf \n
+printf "\n"
+printf "\n"
+printf " _      _  _          _      _     ___  ___\n"
+printf "| |    (_)| |        | |    | |    |  \/  |\n"
+printf "| |     _ | |_   ___ | |    | |    | .  . |\n"
+printf "| |    | || __| / _ \| |    | |    | |\/| |\n"
+printf "| |____| || |_ |  __/| |____| |____| |  | |\n"
+printf "\_____/|_| \__| \___|\_____/\_____/\_|  |_/\n"
+printf "\n                                           "
+printf "\n"
 
 printf "Installation complete. Run `python3 service.py` to start the service"
