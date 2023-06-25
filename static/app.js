ef2d248efde4745d265ea85ccbafc51257ef0f68
tharunoptimus <tharunoptimus@outlook.com>
2023-06-25 11:57:32 +0530

    app.js is now defered

--- static/app.js
+++ static/app.js
@@ -1,16 +1,15 @@
-const LLM_SERVICE_ENDPOINT = "http://localhost:8000/generate"
+const LLM_SERVICE_ENDPOINT = "./generate"
 let submitButton = document.querySelector("#submit")
 let promptTextInput = document.querySelector("#prompt")
 let statusText = document.querySelector("#status")
 let outputTextArea = document.querySelector("#output")
 
-let clearStatus = () => statusText.textContent = ""
-let setStatus = (content) => statusText.textContent = content
-let clearOutput = () =>	outputTextArea.textContent = ""
-let setOutput = (content) => outputTextArea.textContent = content
+let clearStatus = () => (statusText.textContent = "")
+let setStatus = (content) => (statusText.textContent = content)
+let clearOutput = () => (outputTextArea.textContent = "")
+let setOutput = (content) => (outputTextArea.textContent = content)
 
 submitButton.addEventListener("click", async () => {
-	
 	let startTime = new Date().getTime()
 
 	let interval = setInterval(() => {
@@ -18,22 +17,21 @@ submitButton.addEventListener("click", async () => {
 		setStatus(`Running LiteLLM... (${timeLapsed}s)`)
 	})
 
-
 	let inputText = promptTextInput.value
-	if(inputText.trim() == "") return
-	
+	if (inputText.trim() == "") return
+
 	let [response, error] = await getResponse(inputText)
-	
-	if(error) {
+
+	if (error) {
 		setStatus("Error occured while generating text")
 		return
 	}
 
 	let endTime = new Date().getTime()
 	let timeTaken = (endTime - startTime) / 1000
-	
+
 	setOutput(response)
-	
+
 	setStatus(`Done in ${timeTaken} seconds`)
 	clearInterval(interval)
 })
@@ -41,12 +39,11 @@ submitButton.addEventListener("click", async () => {
 promptTextInput.addEventListener("keyup", (event) => {
 	clearStatus()
 	clearOutput()
-	if(event.keyCode == 13) {
+	if (event.keyCode == 13) {
 		event.preventDefault()
 		submitButton.click()
 	}
 })
-
 
 async function getResponse(prompt) {
 	try {
